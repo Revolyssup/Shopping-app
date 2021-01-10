2c0164dc7e6bc3a08e1c6a3308d745014bd339d4
Ashish <ashishjaitiwari15112000@gmail.com>
2021-01-11 01:13:04 +0530

    pooped css

---
+++ client/build/precache-manifest.552bd7b6c33c54a4c9d2651a0fe7574f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c621e253dd4e8d98b6e44d3a50335271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "446276862b433be97dd0",
+    "url": "/static/css/main.3da4a7f2.chunk.css"
+  },
+  {
+    "revision": "11b47f38d7076ac3b5ec",
+    "url": "/static/js/2.fda1b991.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fda1b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446276862b433be97dd0",
+    "url": "/static/js/main.0e7a4948.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
